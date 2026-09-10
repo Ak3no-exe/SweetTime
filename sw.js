4a6a91394b50cbf54998b8e649275ee711185d25
Ak3no-exe <matteosarakha@gmail.com>
2026-09-10 16:23:02 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE="sweettime-v1";
-const ASSETS=["./index.html","./manifest.json","./icon-192.svg","./icon-512.svg"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
-self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))});
